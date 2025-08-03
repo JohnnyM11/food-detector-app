23a71d74e829be704879716ffbf200d41d250362
JohnnyM11 <94123587+JohnnyM11@users.noreply.github.com>
2025-08-03 16:18:24 +0200

    🔄 Deploy dev (2025-08-03 16:18)

--- frontend/src/components/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import "./App.css"; // sicherstellen, dass das CSS eingebunden ist
 
 function LoadingSpinner() {
   return (
     <div className="loading-dots-container">
       <span className="loading-label">Lädt</span>
       <div className="loading-dots">
-        <span></span>
-        <span></span>
-        <span></span>
+        <span className="dot"></span>
+        <span className="dot"></span>
+        <span className="dot"></span>
       </div>
     </div>
   );
