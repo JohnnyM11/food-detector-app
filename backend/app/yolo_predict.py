a778abbebc868dad41d43d7c45f08de3c3e466b3
JohnnyM11 <94123587+JohnnyM11@users.noreply.github.com>
2025-08-14 14:31:16 +0200

    🔄 Deploy dev (2025-08-14 14:31)

--- backend/app/yolo_predict.py
+++ backend/app/yolo_predict.py
@@ -1,6 +1,7 @@
 # app/yolo_predict.py
 
 import io
+from pathlib import Path
 from PIL import Image
 from ultralytics import YOLO
 
@@ -13,7 +14,7 @@ from ultralytics import YOLO
 #MODELL = "yolo11m.pt"
 #MODELL = "yolo11l.pt"
 #MODELL = "yolo11x.pt"
-MODELL = r"models\yolov8n_best.pt"
+MODELL = "models/yolov8n_best.pt"
 model = YOLO(MODELL)
 
 # Funktion ist ein Platzhalter für die spätere YOLO-Erkennung
