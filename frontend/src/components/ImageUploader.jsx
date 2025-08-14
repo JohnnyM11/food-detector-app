7a548cfbdd335b017f929fecea271bf38d00c2d8
JohnnyM11 <94123587+JohnnyM11@users.noreply.github.com>
2025-08-14 15:35:42 +0200

    🔄 Deploy dev (2025-08-14 15:35)

--- frontend/src/components/ImageUploader.jsx
+++ frontend/src/components/ImageUploader.jsx
@@ -29,7 +29,7 @@ function ImageUploader({ onResult, setLoading }) {
   return (
     <div>
       <input type="file" accept="image/*" onChange={handleFileChange} />
-      {"\n"}
+      {"\n\n\n"}
       {image && <img src={image} alt="Preview" width="300" />}
     </div>
   );
