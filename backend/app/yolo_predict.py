f4b5413318cd233a3da480a41d64d63053d4b4e8
JohnnyM11 <94123587+JohnnyM11@users.noreply.github.com>
2025-09-22 02:52:17 +0200

    🔄 Deploy dev (2025-09-22 02:52)

--- backend/app/yolo_predict.py
+++ backend/app/yolo_predict.py
@@ -27,12 +27,12 @@ from pathlib import Path
 #MODELL = "yolov8m.pt"
 #MODELL = "yolo11n.pt"
 #MODELL = "yolo11s.pt"
-#MODELL = "yolo11m.pt"
+MODELL = "yolo11m.pt"
 #MODELL = "yolo11l.pt"
 #MODELL = "yolo11x.pt"
 
 # Eigenes trainiertes Modell im Ordner backend/models/:
-MODELL = "models/yolo11m-best-2007.pt"
+#MODELL = "models/yolo11m-best-2007.pt"
 #MODELL = "models/yolo11n-best-1257.pt"
 #MODELL = "models/yolo11s-best-1625.pt"
 
